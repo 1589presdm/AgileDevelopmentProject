88b75cbcf343524df9381a3db4584076ed43ed75
Joona Mölsä <2306075@edu.karelia.fi>
2025-04-07 18:22:35 +0300

    Navbar muutos

--- dev-karelia-main/routes/initialData.js
+++ dev-karelia-main/routes/initialData.js
@@ -1,11 +1,11 @@
 const users = [
-    {
-        _id: 1587912,
-        api_key: "rwuy6434tgdgjhtiojiosi838tjue3",
-        username: "petri.laitinen@karelia.fi",
-        validUntil: "2024-12-01T09:55:55.851Z",
-        usage: 0
-    }
-]
+  {
+    _id: 1587912,
+    api_key: "rwuy6434tgdgjhtiojiosi838tjue3",
+    username: "petri.laitinen@karelia.fi",
+    validUntil: "2024-12-01T09:55:55.851Z",
+    usage: 0,
+  },
+];
 
 module.exports = { users };
