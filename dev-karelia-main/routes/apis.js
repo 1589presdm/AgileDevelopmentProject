9e8d2e191dec6001ee94bff0d6c50a76ff00554f
Joona Mölsä <2306075@edu.karelia.fi>
2025-05-05 11:11:27 +0300

    apis-tietokantahaku

--- dev-karelia-main/routes/apis.js
+++ dev-karelia-main/routes/apis.js
@@ -1,12 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const { getAllApis } = require("./db");
 
-/* GET apis listing. */
-router.get('/', function(req, res, next) {
-    res.render('apis', { 
-        title: 'Karelia APIs', 
-        isLoggedIn: req.session.user ? true : false
+router.get("/", async function (req, res, next) {
+  try {
+    const apis = await getAllApis();
+    res.render("apis", {
+      title: "Karelia APIs",
+      isLoggedIn: !!req.session.user,
+      apis: apis,
     });
+  } catch (error) {
+    console.error("Virhe haettaessa API-tietoja:", error);
+    res.status(500).send("Tietokantavirhe");
+  }
 });
 
 module.exports = router;
